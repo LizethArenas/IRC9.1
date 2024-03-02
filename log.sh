284d8c84853fb3b5ace8f7d0ff91ed724d0cab08
LizethArenas <157187090+LizethArenas@users.noreply.github.com>
2024-03-02 14:12:19 -0600

    log.sh

--- log.sh
+++ log.sh
@@ -1,14 +1,12 @@
 
 #!/bin/bash
 
-# Obtener porcentaje de uso de la RAM
+
 ram_total=$(free -m | awk '/^Mem/ {print $2}')
 ram_usada=$(free -m | awk '/^Mem/ {print $3}')
 porcentaje_ram=$(echo "scale=2; $ram_usada / $ram_total * 100" | bc)
 
-# Obtener porcentaje de uso de la CPU
 porcentaje_cpu=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | awk -F. '{print $1}')
 
-# Imprimir resultados
 echo "Porcentaje de RAM utilizada: $porcentaje_ram%"
 echo "Porcentaje de CPU utilizada: $porcentaje_cpu%"
